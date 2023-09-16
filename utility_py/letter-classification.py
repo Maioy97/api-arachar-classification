96b0a4d0b874924f43234537ed3e55427023af42
Mai Algendy <mai.h@aptc4i.com>
2023-09-16 20:51:48 +0300

    letter-classification.py: completed

--- utility_py/letter-classification.py
+++ utility_py/letter-classification.py
@@ -2,14 +2,15 @@
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image as image_preprocessing
 from PIL import Image, ImageOps
+import numpy as np
 
 
 class LetterClassification:
     def __init__(self):
-        model_path = "./models/model_weights.h5"
-        model = load_model(model_path)
+        model_path = "../models/model_weights.h5"
+        self.model = load_model(model_path, compile=False)
         self.input_shape = (64, 64)
-        class_labels = [
+        self.class_labels = [
                         'ا',  'ب',  'ت',  'ث',  'ج',  'ح',  'خ',  # خ 6
                         'د',  'ذ',  'ر',  'ز',  'س',  'ش',  'ص',  # ص13
                         'ض',  'ط',  'ظ',  'ع',  'غ',  'ف',  'ق',  # ق 20
@@ -23,19 +24,19 @@ class LetterClassification:
         normalization
         resizing
 
-        :param letter_image:
+        :param letter_image: PIL image object
         :return: pre processed image
         """
-        # pillow image for the rest of the functions
-        letter_image = Image.fromarray(letter_image)
-        mode = letter_image.mode()  # expecting "RGB" or "L" for gray
-
+        mode = letter_image.mode  # expecting "RGB" or "L" for gray
         if mode != "L ":
             # if not gray turn gray
             letter_image = ImageOps.grayscale(letter_image)
+
         # normalize and resize to input size
-        letter_image = letter_image/255
-        letter_image = letter_image.resize(letter_image, self.input_shape)
+        letter_image = letter_image.resize( self.input_shape)
+        letter_image = np.array(letter_image)
+        letter_image = letter_image / 255
+        letter_image = np.reshape(letter_image, (1, self.input_shape[0], self.input_shape[1], 1))
         return letter_image
 
     def classify_letter(self, letter_image):
@@ -46,13 +47,23 @@ class LetterClassification:
         """
         processed_letter_img = self.image_preprocessing(letter_image)
         prediction = self.model(processed_letter_img)
-        result = {"letter": self. class_labels[prediction[0]], "conf": prediction[1] }
+        index = np.argmax(prediction)
+        confidence = prediction[0, index].numpy()
+        result = (self. class_labels[index], confidence)
         return result
-
 
 
 if __name__ == '__main__':
     # debugging
     classifier = LetterClassification()
-    test_image = "./input/data/0_alif/0.png"
-    # import
+    test_image = "../input/dataset/28_lamalif/3059.png"
+    import base64
+
+    with open(test_image, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    from io import BytesIO
+    import base64
+
+    im = Image.open(BytesIO(base64.b64decode(encoded_string)))
+    result = classifier.classify_letter(im)
+    print(result)
